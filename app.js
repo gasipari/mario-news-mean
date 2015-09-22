333eb2333137cdea6e0f423080aa9faa64dce776
Marius Mukunzi <riamarius@gmail.com>
2015-09-21 23:58:44 -0400

    Added authentication with passport module

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport');
 
 // models
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 // connect to the local MongoDB instance
 mongoose.connect('mongodb://localhost/marionews');
@@ -22,13 +25,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+// uncomment after placing favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -28,6 +28,26 @@ app.config([
                         return posts.get($stateParams.id);
                     }]
                 }
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: '/login.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth) {
+                    if (auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }]
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/register.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth) {
+                    if (auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }]
             });
 
         // handle the route if the app receives a URL that is not defined
@@ -35,60 +55,127 @@ app.config([
     }
 ]);
 
-// posts service
-app.factory('posts', ['$http', function($http) {
-    var o = {
-        posts: []
-    };
-    // get all posts
-    o.getAll = function() {
-        return $http.get('/posts').success(function(data) {
-            angular.copy(data, o.posts);
-        });
-    };
-    // get single post
-    o.get = function(id) {
-        return $http.get('/posts/' + id).then(function(res) {
-            return res.data;
-        });
-    };
-    // create new posts
-    o.create = function(post) {
-        return $http.post('/posts', post).success(function(data) {
-            o.posts.push(data);
-        });
-    };
-    // upvoting posts
-    o.upvote = function(post) {
-        return $http.put('/posts/' + post._id + '/upvote')
-            .success(function(data) {
+
+app.factory('auth', ['$http', '$window', function($http, $window) {
+        var auth = {};
+        auth.saveToken = function(token) {
+            $window.localStorage['mario-news-token'] = token;
+        };
+
+        auth.getToken = function() {
+            return $window.localStorage['mario-news-token'];
+        }
+
+        auth.isLoggedIn = function() {
+            var token = auth.getToken();
+
+            if (token) {
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        };
+
+        auth.currentUser = function() {
+            if (auth.isLoggedIn()) {
+                var token = auth.getToken();
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.username;
+            }
+        };
+
+        auth.register = function(user) {
+            return $http.post('/register', user).success(function(data) {
+                auth.saveToken(data.token);
+            });
+        };
+
+        auth.logIn = function(user) {
+            return $http.post('/login', user).success(function(data) {
+                auth.saveToken(data.token);
+            });
+        };
+
+        auth.logOut = function() {
+            $window.localStorage.removeItem('mario-news-token');
+        };
+
+        return auth;
+    }])
+    // posts service
+    .factory('posts', ['$http', 'auth', function($http, auth) {
+        var o = {
+            posts: []
+        };
+        // get all posts
+        o.getAll = function() {
+            return $http.get('/posts').success(function(data) {
+                angular.copy(data, o.posts);
+            });
+        };
+        // get single post
+        o.get = function(id) {
+            return $http.get('/posts/' + id).then(function(res) {
+                return res.data;
+            });
+        };
+        // create new posts
+        o.create = function(post) {
+            return $http.post('/posts', post, {
+                headers: {
+                    Authorization: 'Bearer ' + auth.getToken()
+                }
+            }).success(function(data) {
+                o.posts.push(data);
+            });
+        };
+
+        // upvoting posts
+        o.upvote = function(post) {
+            return $http.put('/posts/' + post._id + '/upvote', null, {
+                headers: {
+                    Authorization: 'Bearer ' + auth.getToken()
+                }
+            }).success(function(data) {
                 post.upvotes += 1;
             });
-    };
-    // adding new comment
-    o.addComment = function(id, comment) {
-        return $http.post('/posts/' + id + '/comments', comment);
-    };
-    // upvoting comments
-    o.upvoteComment = function(post, comment) {
-        return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
-            .success(function(data) {
+        };
+        // adding new comment
+        o.addComment = function(id, comment) {
+            return $http.post('/posts/' + id + '/comments', comment, {
+                headers: {
+                    Authorization: 'Bearer ' + auth.getToken()
+                }
+            });
+        };
+        // upvoting comments
+        o.upvoteComment = function(post, comment) {
+            return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null, {
+                headers: {
+                    Authorization: 'Bearer ' + auth.getToken()
+                }
+            }).success(function(data) {
                 comment.upvotes += 1;
             });
-    };
-    return o;
-}])
+        };
+        return o;
+    }])
 
 // main controller
 app.controller('MainCtrl', [
     '$scope',
     'posts',
-    function($scope, posts) {
+    'auth',
+    function($scope, posts, auth) {
 
         // $scope variable
         $scope.test = 'Hello Marius!';
         // bind $scope.posts variable to the posts array in the posts service
         $scope.posts = posts.posts;
+        $scope.isLoggedIn = auth.isLoggedIn;
 
         // add new post
         $scope.addPost = function() {
@@ -110,13 +197,50 @@ app.controller('MainCtrl', [
     }
 ])
 
+.controller('AuthCtrl', [
+    '$scope',
+    '$state',
+    'auth',
+    function($scope, $state, auth) {
+        $scope.user = {};
+
+        $scope.register = function() {
+            auth.register($scope.user).error(function(error) {
+                $scope.error = error;
+            }).then(function() {
+                $state.go('home');
+            });
+        };
+
+        $scope.logIn = function() {
+            auth.logIn($scope.user).error(function(error) {
+                $scope.error = error;
+            }).then(function() {
+                $state.go('home');
+            });
+        };
+    }
+])
+
+.controller('NavCtrl', [
+    '$scope',
+    'auth',
+    function($scope, auth) {
+        $scope.isLoggedIn = auth.isLoggedIn;
+        $scope.currentUser = auth.currentUser;
+        $scope.logOut = auth.logOut;
+    }
+]);
+
 // posts controller
 app.controller('PostsCtrl', [
     '$scope',
     'posts',
     'post',
-    function($scope, posts, post) {
+    'auth',
+    function($scope, posts, post, auth) {
         $scope.post = post;
+        $scope.isLoggedIn = auth.isLoggedIn;
 
         // add comment
         $scope.addComment = function() {
